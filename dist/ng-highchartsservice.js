9e54a1df81b1044404767d3c5f932e4767fd4472
Gil Desmarais <gd@thepeaklab.com>
2015-01-12 16:09:20 +0100

    feat(): Add Provider to dynamically inject functionNames

--- dist/ng-highchartsservice.js
+++ dist/ng-highchartsservice.js
@@ -1,5 +1,17 @@
 (function () {
   'use strict';
+  var noop = function () {
+  };
+  var functionNames = [];
+  var HighChartsConfigService = function HighChartsConfigService() {
+    this.$get = function () {
+      return { functionNames: functionNames };
+    };
+    // allow injection of functionNames
+    this.setFunctionNames = function (functionNames) {
+      functionNames = functionNames;
+    };
+  };
   /**
    * Define HighchartsServicePluginFactory. It basically is an nullobject,
    * creating an interface from which a HighchartsServicePlugin should inherit via
@@ -7,13 +19,6 @@
    * @return {function} Interface/Nullobject for HighchartServicePlugin
    */
   var HighchartsServicePlugin = function () {
-      var noop = function () {
-      };
-      // TODO: inject functionNames dynamically
-      var functionNames = [
-          'preInit',
-          'isAlive'
-        ];
       return function HighchartsServicePlugin() {
         for (var i = 0; i < functionNames.length; i++) {
           this[functionNames[i]] = noop;
@@ -24,15 +29,19 @@
    * handles (un)registering of plugins and offers a callMethod method to call all registered plugins.
    * @return {object} Public API
    */
-  var HighchartsPluginService = function PluginService() {
-    var plugins = [new window.HighchartsServicePlugin()];
+  var HighchartsPluginService = function HighchartsPluginService() {
+    var plugins = [new HighchartsServicePlugin()];
     var callMethod = function callPlugins(methodName, thisContext, args) {
       for (var i = 0; i < plugins.length; i++) {
         if (!plugins[i]) {
           return false;
         }
         try {
-          plugins[i][methodName].apply(thisContext, args);
+          if (plugins[i][methodName]) {
+            plugins[i][methodName].apply(thisContext, args);
+          } else {
+            console.log('method ' + methodName + ' not available on ', plugins[i]);
+          }
         } catch (e) {
           // if method not implemented or some error happened, do not abort processing.
           console.error(e);
@@ -63,14 +72,7 @@
       unregisterPlugin: unregisterPlugin
     };
   };
-  var HighchartsChartConfig = [function HighchartsChartConfig() {
-      }];
-  var HighchartsDiagram = [function HighchartsDiagram() {
-      }];
-  var HighchartService = [
-      'HighchartsChartConfig',
-      'HighchartsDiagram',
-      function (HighchartsChartConfig, HighchartsDiagram) {
+  var HighchartService = [function (HighchartsChartConfig, HighchartsDiagram) {
         return function HighchartService() {
           if (HighchartsChartConfig) {
           }
@@ -83,11 +85,7 @@
             unregisterPlugin: pluginService.unregisterPlugin
           };
         };
-      }
-    ];
+      }];
   // wire module and its components up with angular
-  angular.module('ng-highchartsservice', [
-    'ngResource',
-    'highcharts-ng'
-  ]).factory('HighchartsService', HighchartService).factory('HighchartsChartConfig', HighchartsChartConfig).factory('HighchartsDiagram', HighchartsDiagram).factory('HighchartsServicePlugin', HighchartsServicePlugin);
+  angular.module('ng-highchartsservice', []).provider('HighChartsConfigService', HighChartsConfigService).factory('HighchartsService', HighchartService).factory('HighchartsServicePlugin', HighchartsServicePlugin);
 }());
--- src/ng-highchartsservice.js
+++ src/ng-highchartsservice.js
@@ -1,38 +1,52 @@
-(function () {
+(function() {
   'use strict';
+  var noop = function() {};
+  var functionNames = [];
+
+  var HighChartsConfigService = function HighChartsConfigService() {
+    this.$get = function() {
+      return {
+        functionNames: functionNames
+      };
+    };
+
+    // allow injection of functionNames
+    this.setFunctionNames = function(functionNames) {
+      functionNames = functionNames;
+    };
+  };
+
   /**
    * Define HighchartsServicePluginFactory. It basically is an nullobject,
    * creating an interface from which a HighchartsServicePlugin should inherit via
    * `HighchartsServicePlugin.call(this);`
    * @return {function} Interface/Nullobject for HighchartServicePlugin
    */
-  var HighchartsServicePlugin = (function () {
-      var noop = function () {
-      };
-      // TODO: inject functionNames dynamically
-      var functionNames = [
-          'preInit',
-          'isAlive'
-        ];
-      return function HighchartsServicePlugin() {
-        for (var i = 0; i < functionNames.length; i++) {
-          this[functionNames[i]] = noop;
-        }
-      };
-    }());
+  var HighchartsServicePlugin = (function() {
+    return function HighchartsServicePlugin() {
+      for (var i = 0; i < functionNames.length; i++) {
+        this[functionNames[i]] = noop;
+      }
+    };
+  }());
+
   /**
    * handles (un)registering of plugins and offers a callMethod method to call all registered plugins.
    * @return {object} Public API
    */
-  var HighchartsPluginService = function PluginService() {
-    var plugins = [new window.HighchartsServicePlugin()];
+  var HighchartsPluginService = function HighchartsPluginService() {
+    var plugins = [new HighchartsServicePlugin()];
     var callMethod = function callPlugins(methodName, thisContext, args) {
       for (var i = 0; i < plugins.length; i++) {
         if (!plugins[i]) {
           return false;
         }
         try {
-          plugins[i][methodName].apply(thisContext, args);
+          if (plugins[i][methodName]) {
+            plugins[i][methodName].apply(thisContext, args);
+          } else {
+            console.log('method ' + methodName + ' not available on ', plugins[i]);
+          }
         } catch (e) {
           // if method not implemented or some error happened, do not abort processing.
           console.error(e);
@@ -63,35 +77,26 @@
       unregisterPlugin: unregisterPlugin
     };
   };
-  var HighchartsChartConfig = [function HighchartsChartConfig() {
-      }];
-  var HighchartsDiagram = [function HighchartsDiagram() {
-      }];
+
   var HighchartService = [
-      'HighchartsChartConfig',
-      'HighchartsDiagram',
-      function (HighchartsChartConfig, HighchartsDiagram) {
-        return function HighchartService() {
-          if (HighchartsChartConfig) {
-          }
-          if (HighchartsDiagram) {
-          }
-          var pluginService = new HighchartsPluginService();
-          return {
-            registerPlugin: pluginService.registerPlugin,
-            callMethod: pluginService.callMethod,
-            unregisterPlugin: pluginService.unregisterPlugin
-          };
+
+    function(HighchartsChartConfig, HighchartsDiagram) {
+      return function HighchartService() {
+        if (HighchartsChartConfig) {}
+        if (HighchartsDiagram) {}
+        var pluginService = new HighchartsPluginService();
+        return {
+          registerPlugin: pluginService.registerPlugin,
+          callMethod: pluginService.callMethod,
+          unregisterPlugin: pluginService.unregisterPlugin
         };
-      }
-    ];
+      };
+    }
+  ];
+
   // wire module and its components up with angular
-  angular.module('ng-highchartsservice', [
-    'ngResource',
-    'highcharts-ng'
-  ])
-  .factory('HighchartsService', HighchartService)
-  .factory('HighchartsChartConfig', HighchartsChartConfig)
-  .factory('HighchartsDiagram', HighchartsDiagram)
-  .factory('HighchartsServicePlugin', HighchartsServicePlugin);
+  angular.module('ng-highchartsservice', [])
+    .provider('HighChartsConfigService', HighChartsConfigService)
+    .factory('HighchartsService', HighchartService)
+    .factory('HighchartsServicePlugin', HighchartsServicePlugin);
 }());
